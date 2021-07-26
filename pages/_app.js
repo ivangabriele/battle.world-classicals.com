f1f8eb1f890f4344bb7c258797b4f33948e9cf8e
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-07-26 02:43:41 +0200

    fix: set correct import path for Around css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/no-unresolved
-import "../assets/around/dist/css/theme.min.css";
+import "../assets/around/css/theme.min.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
