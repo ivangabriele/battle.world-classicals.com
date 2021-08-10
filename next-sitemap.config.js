eb6fb8fc05e6d4db3633dcd7841e06766d0ae3ae
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-08-10 12:41:52 +0200

    build(sitemap): initialize

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// https://github.com/iamvishnusankar/next-sitemap/issues/61#issuecomment-725999452
+
+module.exports = {
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  siteUrl: 'https://battle.world-classicals.com',
+}
