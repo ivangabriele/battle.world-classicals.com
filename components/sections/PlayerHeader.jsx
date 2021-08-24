fc732cb847eac0b8e653fcec70f1940466565452
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-08-24 08:54:16 +0200

    feat: add team & player header background animation

--- components/sections/PlayerHeader.jsx
+++ components/sections/PlayerHeader.jsx
@@ -2,128 +2,76 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-new */
 
-import { useEffect, useRef, useState } from 'react'
+import Coalesce from '@reactnimations/coalesce'
+import { useRef } from 'react'
+import css from 'styled-jsx/css'
 
-class Firefly {
-  constructor(context, width, height) {
-    this.context = context
-    this.width = width
-    this.height = height
-
-    this.init()
+const CoalesceStyle = css.resolve`
+  canvas {
+    position: absolute;
   }
+`
 
-  init() {
-    this.x = Math.random() * this.width
-    this.y = Math.random() * this.height
-    this.s = Math.random() * 2
-    this.ang = Math.random() * 2 * Math.PI
-    this.v = (this.s * this.s) / 4
-
-    this.fireflies = []
-  }
-
-  move() {
-    this.x += this.v * Math.cos(this.ang)
-    this.y += this.v * Math.sin(this.ang)
-    this.ang += (Math.random() * 20 * Math.PI) / 180 - (10 * Math.PI) / 180
-  }
-
-  show() {
-    this.context.beginPath()
-    this.context.arc(this.x, this.y, this.s, 0, 2 * Math.PI)
-    this.context.fillStyle = '#fddba3'
-    this.context.fill()
-  }
-}
-
-class FirefliesAnimation {
-  constructor($window, $canvas, $reference) {
-    this.$window = $window
-    this.$canvas = $canvas
-    this.$reference = $reference
-    this.context = $canvas.getContext('2d')
-
-    this.init()
-    this.update()
-  }
-
-  init() {
-    this.width = this.$reference.offsetWidth
-    this.height = this.$reference.offsetHeight
-    this.fireflies = []
-
-    this.$canvas.width = this.width
-    this.$canvas.height = this.height
-
-    this.context.fillRect(0, 0, this.width, this.height)
-  }
-
-  update() {
-    this.$window.requestAnimationFrame(this.update.bind(this))
-    this.context.clearRect(0, 0, this.width, this.height)
-
-    this.draw()
-  }
-
-  draw() {
-    if (this.fireflies.length < 100) {
-      for (let index = 0; index < 10; index++) {
-        this.fireflies.push(new Firefly(this.context, this.width, this.height))
-      }
-    }
-
-    for (let index = 0; index < this.fireflies.length; index++) {
-      const firefly = this.fireflies[index]
-
-      firefly.move()
-      firefly.show()
-      if (firefly.x < 0 || firefly.x > this.width || firefly.y < 0 || firefly.y > this.height) {
-        this.fireflies.splice(index, 1)
-      }
-    }
-  }
-}
-
-export default function TeamHeader({ name }) {
-  const canvasRef = useRef(null)
+export default function PlayerHeader({ name }) {
   const sectionRef = useRef(null)
-  const [hasRendered, setHasRendered] = useState(false)
-
-  useEffect(() => {
-    const $canvas = canvasRef.current
-    const $section = sectionRef.current
-
-    if (!process.browser || hasRendered || $canvas === null || $section === null) {
-      return
-    }
-
-    const firefliesAnimation = new FirefliesAnimation(window, $canvas, $section)
-    window.addEventListener('resize', firefliesAnimation.init.bind(firefliesAnimation))
-
-    setHasRendered(true)
-  }, [hasRendered])
 
   return (
     <>
-      <section ref={sectionRef} className="bg-dark bg-size-cover overflow-hidden ">
-        <canvas ref={canvasRef} />
-        <div className="d-flex justify-content-center align-items-center pt-5 pt-md-6 pt-lg-7 pb-5">
-          {/* <h1 className="d-inline-flex display-5 mb-5 text-light">&#8203;</h1> */}
-          <h1 className="d-inline-flex display-5 mb-5 text-light">{name}</h1>
+      <section ref={sectionRef} className="bg-dark bg-size-cover overflow-hidden hero">
+        <Coalesce
+          baseRef={sectionRef}
+          className={CoalesceStyle.className}
+          settings={{
+            backgroundColor: '#121117',
+            baseHue: 255,
+            baseSpeed: 0.05,
+            opacity: 0.3,
+          }}
+        />
+
+        <div className="container position-relative zindex-5">
+          <div className="d-flex justify-content-center align-items-center pb-6 pt-6">
+            <span aria-hidden="true">&nbsp;</span>
+          </div>
+          <div className="position-absolute d-flex justify-content-center align-items-center test">
+            <h1>{name.replace(/H/gi, 'H')}</h1>
+          </div>
         </div>
       </section>
 
+      {CoalesceStyle.styles}
       <style jsx>{`
-        section {
+        .test {
+          height: 100%;
+          top: 0;
+          width: 100%;
         }
-
-        canvas {
-          position: absolute;
+        span {
+          display: block;
+          font-size: 300%;
         }
-
         h1 {
-          /*text-shadow: 0 0 3rem yellow;*/
+          /*animation: h1 2s ease-out 0s;*/
+          color: white;
+          font-weight: 400;
+          font-size: 250%;
+          letter-spacing: 0.25rem;
+          margin-bottom: 0;
+          max-width: 80%;
+          opacity: 1;
+          text-align: center;
+          text-transform: uppercase;
+        }
+
+        @keyframes h1 {
+          0% {
+            filter: blur(0.125rem);
+            font-size: 200%;
+          }
+          100% {
+            filter: blur(0);
+            font-size: 300%;
+          }
         }
       `}</style>
     </>
--- components/sections/TeamHeader.jsx
+++ components/sections/TeamHeader.jsx
@@ -1,20 +1,74 @@
+import Coalesce from '@reactnimations/coalesce'
+import { useRef } from 'react'
+import css from 'styled-jsx/css'
+
+const CoalesceStyle = css.resolve`
+  canvas {
+    position: absolute;
+  }
+`
+
 export default function TeamHeader({ name }) {
+  const sectionRef = useRef(null)
+
   return (
     <>
-      <section className="bg-dark bg-size-cover overflow-hidden pt-5 pt-md-6 pt-lg-7 pb-5">
-        <div className="d-flex justify-content-center align-items-center">
-          <h1 className="d-inline-flex display-5 mb-5 text-light">{name}</h1>
+      <section ref={sectionRef} className="bg-dark bg-size-cover overflow-hidden hero">
+        <Coalesce
+          baseRef={sectionRef}
+          className={CoalesceStyle.className}
+          settings={{
+            backgroundColor: '#121117',
+            baseHue: 100,
+            baseSpeed: 0.05,
+            opacity: 0.3,
+            rangeSpeed: 0.5,
+          }}
+        />
+
+        <div className="container position-relative zindex-5">
+          <div className="d-flex justify-content-center align-items-center pb-6 pt-6">
+            <span aria-hidden="true">&nbsp;</span>
+          </div>
+          <div className="position-absolute d-flex justify-content-center align-items-center test">
+            <h1>{name.replace(/H/gi, 'H')}</h1>
+          </div>
         </div>
       </section>
 
+      {CoalesceStyle.styles}
       <style jsx>{`
-        section {
-          background-image: url('/photos/hero.png');
-          background-position: 0 10%;
+        .test {
+          height: 100%;
+          top: 0;
+          width: 100%;
+        }
+        span {
+          display: block;
+          font-size: 300%;
+        }
+        h1 {
+          /*animation: h1 2s ease-out 0s;*/
+          color: white;
+          font-weight: 400;
+          font-size: 250%;
+          letter-spacing: 0.25rem;
+          margin-bottom: 0;
+          max-width: 80%;
+          opacity: 1;
+          text-align: center;
+          text-transform: uppercase;
         }
 
-        h1 {
-          text-shadow: 0 0 3rem yellow;
+        @keyframes h1 {
+          0% {
+            filter: blur(0.125rem);
+            font-size: 200%;
+          }
+          100% {
+            filter: blur(0);
+            font-size: 300%;
+          }
         }
       `}</style>
     </>
