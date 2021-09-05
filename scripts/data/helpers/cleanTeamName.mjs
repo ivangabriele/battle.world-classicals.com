aa227fe543e1dd2e76a78c6644fd964a253f2d10
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-09-05 17:38:27 +0200

    build(data): remove non-word-like chars from team names

--- scripts/data/helpers/cleanTeamName.mjs
+++ scripts/data/helpers/cleanTeamName.mjs
@@ -8,17 +8,27 @@ function capitalize(sentence) {
     .join(' ')
 }
 
+function cleanNonLetterChars(sentence) {
+  return sentence.replace(/[❤♟,_]/g, ' ')
+}
+
 function cleanEmojis(sentence) {
-  return sentence.replace(emojiRegex(), '')
+  return sentence.replace(emojiRegex(), ' ')
+}
+
+function cleanParentheses(sentence) {
+  return sentence.replace(/\([^(]*\)/g, ' ')
 }
 
 function trim(sentence) {
-  return sentence.replace(/\s+/, ' ').trim()
+  return sentence.replace(/\s+/g, ' ').trim()
 }
 
 export default function cleanTeamName(rawName) {
   const nameWithoutEmojis = cleanEmojis(rawName)
-  const nameCapitalized = capitalize(nameWithoutEmojis)
+  const nameWithoutNonLetterChars = cleanNonLetterChars(nameWithoutEmojis)
+  const nameWithoutParentheses = cleanParentheses(nameWithoutNonLetterChars)
+  const nameCapitalized = capitalize(nameWithoutParentheses)
   const nameTrimmed = trim(nameCapitalized)
   const name = nameTrimmed
 
