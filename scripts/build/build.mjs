e3fa293c2905336081d4a42c000da9a3a56d8856
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-09-16 01:27:05 +0200

    build(vercel): download LFS directories from Github release

---
+++ scripts/build/build.mjs
@@ -0,0 +1,42 @@
+/**
+ * Vercel doesn't support Git LFS, therefore this custom build script
+ *
+ * @see https://github.com/vercel/vercel/discussions/3716
+ */
+
+import shelljs from 'shelljs'
+
+const { NODE_ENV, npm_package_version: VERSION, VERCEL } = process.env
+const IS_PROD = NODE_ENV === 'production'
+const IS_VERCEL = VERCEL !== undefined
+const RELEASES_DOWNLOAD_URL = 'https://github.com/ivangabriele/battle.world-classicals.com/releases/download'
+
+function run(command) {
+  shelljs.echo(`Running: \`${command}\`…`)
+  const output = shelljs.exec(command)
+
+  if (output.code !== 0) {
+    shelljs.exit(1)
+  }
+}
+
+;(() => {
+  try {
+    if (IS_VERCEL) {
+      run(`curl -s -L ${RELEASES_DOWNLOAD_URL}/v${VERSION}/data.tar.gz -o ./data.tar.gz`)
+      run(`curl -s -L ${RELEASES_DOWNLOAD_URL}/v${VERSION}/public.tar.gz -o ./public.tar.gz`)
+      run(`tar -x --file=data.tar.gz`)
+      run(`tar -x --file=public.tar.gz`)
+    }
+
+    run(`yarn next build`)
+
+    if (IS_PROD) {
+      run(`yarn next-sitemap --config next-sitemap.config.js`)
+    }
+  } catch (err) {
+    shelljs.echo(`[scripts/build.js] Error: ${err.message}`)
+
+    shelljs.exit(1)
+  }
+})()
